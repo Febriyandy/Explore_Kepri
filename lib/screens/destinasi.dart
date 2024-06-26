1b25acb8f5df6b201cf2a713e3fff368e7c9abfd
Febriandii <febriandinfi@gmail.com>
2024-06-27 00:32:25 +0700

    destinasi

--- lib/screens/destinasi.dart
+++ lib/screens/destinasi.dart
@@ -1,9 +1,9 @@
 import 'dart:ui';
-
 import 'package:explore_kepri/screens/landing.dart';
 import 'package:explore_kepri/utils/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:firebase_database/firebase_database.dart';
 
 class DestinasiPage extends StatefulWidget {
   const DestinasiPage({super.key});
@@ -13,6 +13,32 @@ class DestinasiPage extends StatefulWidget {
 }
 
 class _DestinasiPageState extends State<DestinasiPage> {
+  final DatabaseReference _destinasiRef =
+      FirebaseDatabase.instance.ref().child('explore-kepri/destinasi');
+
+  List<Map<dynamic, dynamic>> destinasiList = [];
+  @override
+  void initState() {
+    super.initState();
+    _destinasiRef.onValue.listen((event) {
+      var snapshot = event.snapshot;
+      var data = snapshot.value as Map<dynamic,
+          dynamic>?; // Tambahkan tanda tanya (?) setelah snapshot.value
+      if (data != null) {
+        List<Map<dynamic, dynamic>> tempDestinasiList = [];
+        data.forEach((key, value) {
+          tempDestinasiList.add(value as Map<dynamic, dynamic>);
+        });
+        setState(() {
+          destinasiList = tempDestinasiList;
+        });
+      } else {
+        // Handle case when snapshot.value is null or empty
+        print('Data from Firebase is null or empty');
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -34,6 +60,7 @@ class _DestinasiPageState extends State<DestinasiPage> {
                 SizedBox(
                   height: MediaQuery.of(context).padding.top,
                 ),
+                // Header with Back button and Logo
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   crossAxisAlignment: CrossAxisAlignment.center,
@@ -49,6 +76,7 @@ class _DestinasiPageState extends State<DestinasiPage> {
                         },
                         child: SvgPicture.asset(
                           'assets/icons/back.svg',
+                          // ignore: deprecated_member_use
                           color: blueColor,
                           width: 30.0,
                           height: 30.0,
@@ -67,6 +95,7 @@ class _DestinasiPageState extends State<DestinasiPage> {
                     ),
                   ],
                 ),
+                // Search Bar
                 Padding(
                   padding: const EdgeInsets.fromLTRB(20, 20, 20, 0),
                   child: Container(
@@ -117,13 +146,12 @@ class _DestinasiPageState extends State<DestinasiPage> {
                     ),
                   ),
                 ),
-
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
                     Padding(
-                      padding: EdgeInsets.fromLTRB(20, 20, 0, 0),
+                      padding: const EdgeInsets.fromLTRB(20, 20, 0, 0),
                       child: Column(
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: [
@@ -131,7 +159,7 @@ class _DestinasiPageState extends State<DestinasiPage> {
                             "Destinasi",
                             style: TextStyle(
                               color: darkColor,
-                              fontSize: 18,
+                              fontSize: 16,
                               fontWeight: FontWeight.bold,
                               fontFamily: "Poppins",
                             ),
@@ -145,181 +173,106 @@ class _DestinasiPageState extends State<DestinasiPage> {
                         onTap: () {},
                         child: SvgPicture.asset(
                           'assets/icons/filter.svg',
+                          // ignore: deprecated_member_use
                           color: blueColor,
-                          width: 30.0,
-                          height: 30.0,
+                          width: 25.0,
+                          height: 25.0,
                         ),
                       ),
                     ),
                   ],
                 ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Padding(
-                      padding: EdgeInsets.fromLTRB(20, 20, 0, 0),
-                      child: Stack(
-                        children: [
-                          Container(
-                            height: 220,
-                            width: 165,
-                            decoration: BoxDecoration(
-                              color: Colors.white.withOpacity(0.2),
-                              borderRadius: BorderRadius.circular(15.0),
-                              border: Border.all(
-                                color: Colors.white.withOpacity(1),
-                              ),
-                            ),
-                            child: ClipRRect(
-                              borderRadius: BorderRadius.circular(15.0),
-                              child: Image.asset(
-                                'assets/images/cover1.jpg',
+                // Destinasi Cards
+                GridView.count(
+                  crossAxisCount: 2,
+                  shrinkWrap: true,
+                  physics: const NeverScrollableScrollPhysics(),
+                  padding:
+                    const  EdgeInsets.symmetric(horizontal: 20.0, vertical: 15.0),
+                  childAspectRatio: 0.8, // Ratio aspect untuk gambar
+                  mainAxisSpacing: 20.0,
+                  crossAxisSpacing: 20.0,
+                  children: destinasiList.map((destinasi) {
+                    return  Stack(
+                      children: [
+                        Container(
+                          decoration: BoxDecoration(
+                            color: Colors.white.withOpacity(0.2),
+                            borderRadius: BorderRadius.circular(15.0),
+                          ),
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(15.0),
+                            child: AspectRatio(
+                              aspectRatio:
+                                  0.8, // Sesuaikan dengan childAspectRatio di GridView
+                              child: Image.network(
+                                destinasi['images'][0],
                                 fit: BoxFit.cover,
                               ),
                             ),
                           ),
-                          Positioned(
-                            bottom: 0,
-                            left: 0,
-                            right: 0,
-                            child: ClipRRect(
-                              borderRadius: const BorderRadius.only(
-                                bottomLeft: Radius.circular(15.0),
-                                bottomRight: Radius.circular(15.0),
-                              ),
-                              child: BackdropFilter(
-                                filter: ImageFilter.blur(sigmaX: 2, sigmaY: 2),
-                                child: Container(
-                                  height: 65,
-                                  decoration: const BoxDecoration(
-                                    color: Colors.transparent,
-                                    borderRadius: BorderRadius.only(
-                                      bottomLeft: Radius.circular(15.0),
-                                      bottomRight: Radius.circular(15.0),
-                                    ),
+                        ),
+                        Positioned(
+                          bottom: 0,
+                          left: 0,
+                          right: 0,
+                          child: ClipRRect(
+                            borderRadius: const BorderRadius.only(
+                              bottomLeft: Radius.circular(15.0),
+                              bottomRight: Radius.circular(15.0),
+                            ),
+                            child: BackdropFilter(
+                              filter: ImageFilter.blur(sigmaX: 2, sigmaY: 2),
+                              child: Container(
+                                height: 55,
+                                decoration: const BoxDecoration(
+                                  color: Colors.transparent,
+                                  borderRadius: BorderRadius.only(
+                                    bottomLeft: Radius.circular(15.0),
+                                    bottomRight: Radius.circular(15.0),
                                   ),
-                                  child: const Padding(
-                                    padding: EdgeInsets.symmetric(
-                                        horizontal: 10.0, vertical: 10.0),
-                                    child: Align(
-                                      alignment: Alignment.centerLeft,
-                                      child: Column(
-                                        mainAxisAlignment:
-                                            MainAxisAlignment.center,
-                                        crossAxisAlignment:
-                                            CrossAxisAlignment.start,
-                                        children: [
-                                          Text(
-                                            'Nama Tempat',
-                                            style: TextStyle(
-                                              color: Colors.white,
-                                              fontSize: 12,
-                                              fontWeight: FontWeight.bold,
-                                            ),
-                                          ),
-                                          Text(
-                                            'Kabupaten',
-                                            style: TextStyle(
-                                              color: Colors.white,
-                                              fontSize: 12,
-                                            ),
-                                          ),
-                                        ],
+                                ),
+                                child: Padding(
+                                  padding: const EdgeInsets.symmetric(
+                                    horizontal: 10.0,
+                                    vertical: 10.0,
+                                  ),
+                                  child: Column(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.center,
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        destinasi['nama_tempat'],
+                                        style: const TextStyle(
+                                          color: Colors.white,
+                                          fontSize: 12,
+                                          fontWeight: FontWeight.bold,
+                                        ),
                                       ),
-                                    ),
+                                      Text(
+                                        destinasi['kabupaten'],
+                                        style: const TextStyle(
+                                          color: Colors.white,
+                                          fontSize: 12,
+                                        ),
+                                      ),
+                                    ],
                                   ),
                                 ),
                               ),
                             ),
                           ),
-                        ],
-                      ),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.fromLTRB(0, 20, 20, 0),
-                      child: Stack(
-                        children: [
-                          Container(
-                            height: 220,
-                            width: 165,
-                            decoration: BoxDecoration(
-                              color: Colors.white.withOpacity(0.2),
-                              borderRadius: BorderRadius.circular(15.0),
-                              border: Border.all(
-                                color: Colors.white.withOpacity(1),
-                              ),
-                            ),
-                            child: ClipRRect(
-                              borderRadius: BorderRadius.circular(15.0),
-                              child: Image.asset(
-                                'assets/images/cover.jpeg',
-                                fit: BoxFit.cover,
-                              ),
-                            ),
-                          ),
-                          Positioned(
-                            bottom: 0,
-                            left: 0,
-                            right: 0,
-                            child: ClipRRect(
-                              borderRadius: const BorderRadius.only(
-                                bottomLeft: Radius.circular(15.0),
-                                bottomRight: Radius.circular(15.0),
-                              ),
-                              child: BackdropFilter(
-                                filter: ImageFilter.blur(sigmaX: 2, sigmaY: 2),
-                                child: Container(
-                                  height: 65,
-                                  decoration: const BoxDecoration(
-                                    color: Colors.transparent,
-                                    borderRadius: BorderRadius.only(
-                                      bottomLeft: Radius.circular(15.0),
-                                      bottomRight: Radius.circular(15.0),
-                                    ),
-                                  ),
-                                  child: const Padding(
-                                    padding: EdgeInsets.symmetric(
-                                        horizontal: 10.0, vertical: 10.0),
-                                    child: Align(
-                                      alignment: Alignment.centerLeft,
-                                      child: Column(
-                                        mainAxisAlignment:
-                                            MainAxisAlignment.center,
-                                        crossAxisAlignment:
-                                            CrossAxisAlignment.start,
-                                        children: [
-                                          Text(
-                                            'Nama Tempat',
-                                            style: TextStyle(
-                                              color: Colors.white,
-                                              fontSize: 12,
-                                              fontWeight: FontWeight.bold,
-                                            ),
-                                          ),
-                                          Text(
-                                            'Kabupaten',
-                                            style: TextStyle(
-                                              color: Colors.white,
-                                              fontSize: 12,
-                                            ),
-                                          ),
-                                        ],
-                                      ),
-                                    ),
-                                  ),
-                                ),
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
+                        ),
+                      ],
+                    );
+                  }).toList(),
                 ),
-
-                SizedBox(
-                    height:
-                        100), // Padding bawah agar konten tidak tertutup oleh bottom navigation
+                const SizedBox(
+                  height:
+                      100, // Padding bawah agar konten tidak tertutup oleh bottom navigation
+                ),
               ],
             ),
           ),
